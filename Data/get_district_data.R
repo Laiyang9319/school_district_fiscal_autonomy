84e70d470a6708b203153c5d66b4d2d590f6f96c
Laiyang19 <kelaiyang9319@hotmail.com>
2023-06-10 16:10:50 -0400

    Update code to get finance data

--- Data/get_district_data.R
+++ Data/get_district_data.R
@@ -5,7 +5,18 @@ directory <- get_education_data(level = "school-districts",
                            filters = list(year = c(1993:2021)))
 write.csv(directory, file = "districts_directory.csv", row.names = FALSE)
 
-finance <- get_education_data(level = "school-districts",
-                              source = "ccd",
-                              topic = "finance",
-                              filters = list(year = c(1994:2018)))
+finance <- data.frame()
+
+# loop through the years
+for(i in 1996:1998) {
+  
+  # get data for the year i
+  getfinance <- get_education_data(level = "school-districts",
+                                source = "ccd",
+                                topic = "finance",
+                                filters = list(year = i))
+  
+  # combine the new data with the existing data
+  finance <- rbind(all_data, getfinance)
+}
+
